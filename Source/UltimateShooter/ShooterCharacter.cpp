ab815be9bdfffbf160ccf370953affae24e14f81
Mert Oyman <mertoyman@gmail.com>
2021-09-05 19:22:57 +0300

    Socket offset for better view

--- Source/UltimateShooter/ShooterCharacter.cpp
+++ Source/UltimateShooter/ShooterCharacter.cpp
@@ -24,6 +24,7 @@ AShooterCharacter::AShooterCharacter() :
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->TargetArmLength = 300.f;
 	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+	CameraBoom->SocketOffset = FVector(0.f, 50.f, 50.f);
 
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
 	FollowCamera->SetupAttachment(CameraBoom);
@@ -31,10 +32,10 @@ AShooterCharacter::AShooterCharacter() :
 
 	//Do not rotate when controller rotates. Let the controller only affect the camera.
 	bUseControllerRotationPitch = false;
-	bUseControllerRotationYaw = false;
+	bUseControllerRotationYaw = true;
 	bUseControllerRotationRoll = false;
 
-	GetCharacterMovement()->bOrientRotationToMovement = true; //Character moves in the direction of input...
+	GetCharacterMovement()->bOrientRotationToMovement = false; //Character does not move in the direction of input...
 	GetCharacterMovement()->RotationRate = FRotator(0.f, 540.f, 0.f); //... at this rotation rate
 	GetCharacterMovement()->JumpZVelocity = 600.f;
 	GetCharacterMovement()->AirControl = 0.2f;
