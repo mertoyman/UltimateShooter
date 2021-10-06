7654bae6ed5050946a1b3df74fe4f96843188e6d
Mert Oyman <mertoyman@gmail.com>
2021-10-06 20:33:18 +0300

    Add Widget to Weapon

--- Source/UltimateShooter/Item.h
+++ Source/UltimateShooter/Item.h
@@ -32,5 +32,9 @@ private:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Item Properties", meta= (AllowPrivateAccess=true))
 	class UBoxComponent* CollisionBox;
 
+	/* Pickup widget for when the player looks at the item */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Item Properties", meta= (AllowPrivateAccess=true))
+	class UWidgetComponent* PickupWidget;
+
 public:
 };
