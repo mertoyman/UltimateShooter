0fa492b536409bf0eaa93225b56cb927dbbd8948
Mert Oyman <mertoyman@gmail.com>
2021-11-02 01:13:46 +0300

    Ammo class

---
+++ Source/UltimateShooter/Ammo.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ammo.h"
+
+#include "Components/BoxComponent.h"
+#include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+
+AAmmo::AAmmo()
+{
+	// Construct the AmmoMesh component and set it as the root	
+	AmmoMesh = CreateDefaultSubobject<UStaticMeshComponent>("AmmoMesh");
+	SetRootComponent(AmmoMesh);
+
+	GetCollisionBox()->SetupAttachment(GetRootComponent());
+	GetPickupWidget()->SetupAttachment(GetRootComponent());
+	GetAreaSphere()->SetupAttachment(GetRootComponent());
+}
+
+void AAmmo::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AAmmo::Tick(float delta_time)
+{
+	Super::Tick(delta_time);
+}
+
+
