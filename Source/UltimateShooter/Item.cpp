3e3e56936dadb0d4e5c38461a51c5d0024ef9a3c
Mert Oyman <mertoyman@gmail.com>
2021-11-16 16:14:31 +0300

    Material pulse when interping

--- Source/UltimateShooter/Item.cpp
+++ Source/UltimateShooter/Item.cpp
@@ -232,6 +232,7 @@ void AItem::FinishInterping()
 		// Subtract 1 from the ItemCount of the interp location struct
 		Character->IncrementInterpLocItemCount(InterpLocIndex, -1);
 		Character->GetPickupItem(this);
+		SetItemState(EItemState::EIS_PickedUp);
 	}
 
 	// Set scale back to normal
@@ -368,13 +369,29 @@ void AItem::EnableGlowMaterial()
 
 void AItem::UpdatePulse()
 {
-	if (ItemState != EItemState::EIS_Pickup) return;
-
-	const float ElapsedTime{ GetWorldTimerManager().GetTimerElapsed(PulseTimer) };
-	if (PulseCurve)
+	float ElapsedTime{};
+	FVector CurveValue{};
+	
+	switch (ItemState)
 	{
-		const FVector CurveValue{ PulseCurve->GetVectorValue(ElapsedTime) };
+		case EItemState::EIS_Pickup:
+			if (PulseCurve)
+			{
+				ElapsedTime = GetWorldTimerManager().GetTimerElapsed(PulseTimer);
+				CurveValue = PulseCurve->GetVectorValue(ElapsedTime);
+			}
+			break;
+		case EItemState::EIS_EquipInterping:
+			if (InterpPulseCurve)
+			{
+				ElapsedTime = GetWorldTimerManager().GetTimerElapsed(ItemInterpTimer);
+				CurveValue = InterpPulseCurve->GetVectorValue(ElapsedTime);
+			}
+			break;
+	}
 
+	if (DynamicMaterialInstance)
+	{
 		DynamicMaterialInstance->SetScalarParameterValue(TEXT("GlowAmount"), CurveValue.X * GlowAmount);
 		DynamicMaterialInstance->SetScalarParameterValue(TEXT("FresnelExponent"), CurveValue.Y * FresnelExponent);
 		DynamicMaterialInstance->SetScalarParameterValue(TEXT("FresnelReflectFraction"), CurveValue.Z * FresnelReflectFraction);
@@ -443,6 +460,8 @@ void AItem::StartItemCurve(AShooterCharacter* Char)
 
 	SetItemState(EItemState::EIS_EquipInterping);
 
+	GetWorldTimerManager().ClearTimer(PulseTimer);
+	
 	GetWorldTimerManager().SetTimer(ItemInterpTimer, this, &AItem::FinishInterping, ZCurveTime);
 
 	// Get initial yaw of the camera
