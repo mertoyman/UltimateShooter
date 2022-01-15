b643c59fe297f72a5797d8f0b84c27bb8c72fa4b
Mert Oyman <mertoyman@gmail.com>
2022-01-15 16:25:54 +0300

    Enemy run animation

--- Source/UltimateShooter/GruxAnimInstance.cpp
+++ Source/UltimateShooter/GruxAnimInstance.cpp
@@ -3,3 +3,18 @@
 
 #include "GruxAnimInstance.h"
 
+#include "Enemy.h"
+
+void UGruxAnimInstance::UpdateAnimationProperties(float DeltaTime)
+{
+	if (!Enemy)
+	{
+		Enemy = Cast<AEnemy>(TryGetPawnOwner());
+	}
+	else
+	{
+		FVector Velocity { Enemy->GetVelocity() };
+		Velocity.Z = 0.f;
+		Speed = Velocity.Size();
+	}
+}
