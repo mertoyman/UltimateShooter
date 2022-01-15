354f3dac4c83e60786b104c7ac583662b9bf1617
Mert Oyman <mertoyman@gmail.com>
2022-01-15 16:36:22 +0300

    Second patrol point

--- Source/UltimateShooter/Enemy.h
+++ Source/UltimateShooter/Enemy.h
@@ -103,6 +103,10 @@ private:
 	UPROPERTY(EditAnywhere, Category="Behavior Tree", meta=(AllowPrivateAccess = true, MakeEditWidget = true))
 	FVector PatrolPoint;
 
+	/* Point for the enemy to move to */
+	UPROPERTY(EditAnywhere, Category="Behavior Tree", meta=(AllowPrivateAccess = true, MakeEditWidget = true))
+	FVector PatrolPoint2;
+
 	class AEnemyController* EnemyController;
 
 public:	
